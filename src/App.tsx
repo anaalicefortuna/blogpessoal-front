90c9dc4700b71b5aa48293beadd6f180285760c4
anaalicefortuna <anaalicefalves@gmail.com>
2022-12-05 10:46:26 -0300

    feat: tema, postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import CadastroPost from './components/postagem/cadastrar/CadastrarPost';
+import DeletarPostagem from './components/postagem/deletar/DeletarPost';
+import ListaPostagem from './components/postagem/lista/ListaPost';
 import Footer from './components/statics/footer/Footer';
 import Navbar from './components/statics/navbar/NavBar';
+import CadastroTema from './components/tema/cadatro/CadastroTema';
+import DeletarTema from './components/tema/deletar/DeletarTema';
+import ListaTema from './components/tema/lista/ListaTema';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -18,9 +24,27 @@ function App() {
 
         <Route path="/home" element={<Home />} />
 
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+
+        <Route path="/login" element={<Login /> }/>
 
         <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/posts" element={<ListaPostagem />} />
+
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+        <Route path="/formularioTema" element={<CadastroTema />} />
+
+       <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+       <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+       <Route path="/deletarTema/:id" element={<DeletarTema />} />
 
       </Routes>
       <Footer />
